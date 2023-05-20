2a54ca734344807e0e00bbbcac3f58b7f0fa6cf8
João Ramos <Joao.silvaramos2013@hotmail.com>
2023-05-20 18:28:53 -0300

    Realizado a criação da animação dos elementos, correção do problema da responsividade com o zoom, adicionando max:width padrão de 1920px, criado a nova section de habilidades e resolvido problemas de tags abertas no html

--- content/javaScript/script.js
+++ content/javaScript/script.js
@@ -10,3 +10,25 @@ menuMobile.addEventListener('click', function()  {
     }
 });
 
+
+const target = document.querySelectorAll("[data-animation]");
+const animationClass = "animate";
+
+
+function animeScroll() {
+    const windowTop = window.pageYOffset + ((window.innerHeight * 3) / 4);
+    target.forEach(function(e){
+        if((windowTop) > e.offsetTop){
+            e.classList.add(animationClass);
+        }else {
+            e.classList.remove(animationClass);
+        }
+
+
+        console.log(e.offsetTop);
+    });
+}
+
+window.addEventListener('scroll', function() {
+    animeScroll()
+})
